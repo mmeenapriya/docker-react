492930c3b4e26478b81cdb7c62f5809b9b00c6a6
meenapriya <meenapriya.meenakshisundaram@concur.com>
2019-04-03 19:36:05 -0500

    changed to app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>who r u??? </p>
+          <p>I am changed on feature </p>
           <a
             className="App-link"
             href="https://reactjs.org"
